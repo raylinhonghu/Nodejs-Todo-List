9907e1fe1d47773537fa6faccd1e55cdb73f4122
raylinhonghu <raylinhonghu@gmail.com>
2018-02-08 17:49:00 -0800

    MVC structure initialized

---
+++ controllers/todoController.js
@@ -0,0 +1,15 @@
+// render views & passing data to views
+module.exports = function(app){
+
+  app.get('/todo',function(req,res){
+
+  });
+
+  app.post('/todo',function(req,res){
+
+  });
+
+  app.delete('/todo', function(req,res){
+
+  })
+};
